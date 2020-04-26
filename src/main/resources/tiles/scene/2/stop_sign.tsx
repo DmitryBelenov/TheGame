a7126086c6fc6eef2ceda92a6ed0849cce55cff8
DmitryBelenov <dmitrij_belenov@mail.ru>
2020-04-26 14:18:18 +0300

    new

---
+++ src/main/resources/tiles/scene/2/stop_sign.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="stop_sign" tilewidth="8" tileheight="8" tilecount="9" columns="3">
+ <image source="stop_sign.png" width="24" height="24"/>
+</tileset>
